ae7b2bd2d77f616e25ced3599160325709ffb66c
Gediminas Bivainis <gediminas@bivainis.com>
2016-01-14 17:26:53 +0100

    Replace stylus with scss

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,15 @@
-
 module.exports = {
-  devtool: 'sourcemap',
-  output: {
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [
-       { test: /\.js$/, exclude: [/app\/lib/, /node_modules/], loader: 'ng-annotate!babel' },
-       { test: /\.html$/, loader: 'raw' },
-       { test: /\.styl$/, loader: 'style!css!stylus' },
-       { test: /\.css$/, loader: 'style!css' }
-    ]
-  }
+    devtool: 'sourcemap',
+    output: {
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {test: /\.js$/, exclude: [/app\/lib/, /node_modules/], loader: 'ng-annotate!babel'},
+            {test: /\.html$/, loader: 'raw'},
+            //{ test: /\.styl$/, loader: 'style!css!stylus' },
+            {test: /\.(sass|scss)$/, loader: 'style!css!sass'},
+            {test: /\.css$/, loader: 'style!css'}
+        ]
+    }
 };
